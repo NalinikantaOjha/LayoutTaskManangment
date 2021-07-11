e365cb0c1b7d099f3875c5d7edc5205507639be4
nalin <nalinikanta7978@gmail.com>
2021-07-11 14:36:41 +0530

    c1

---
+++ app/src/main/java/com/example/taskmanagmentevening/Homecardexpanded.java
@@ -0,0 +1,14 @@
+package com.example.taskmanagmentevening;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Homecardexpanded extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homecardexpanded);
+    }
+}
